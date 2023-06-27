7198247f85e24ae1aa9a0a8452c7f132f82ba7e3
Andrea Bonomi <andrea.bonomi@gmail.com>
2023-06-27 08:59:32 +0200

    update aliases

--- aliases.sh
+++ aliases.sh
@@ -1,11 +1,14 @@
-alias ls='ls --color=auto'
+alias ls='ls --color=auto -F'
+alias ll='ls --color=auto -lh'
+alias lt='ls --human-readable --size -1 -S --classify'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
+alias df.='df -h .'
+alias free='free -mht'
 
 alias vi=nvim
 alias vim=nvim
 
-alias helm='microk8s helm'
 alias k=kubectl
 complete -o default -F __start_kubectl k
