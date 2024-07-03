658f16047439ce5cc4046f5083be829552619094
Andrea Bonomi <andrea.bonomi@gmail.com>
2024-07-03 09:29:39 +0200

    move install scripts to install.d directory

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@ set -e
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
 mv ~/.bashrc ~/.bashrc.old 2>/dev/null || true
 ln -s $(readlink -f bashrc) ~/.bashrc
-./install_git_aliases.sh
-./install_zoxide.sh
+# Execute install commands
+for install in ~/.config/bash/install.d/* ; do
+  $install
+done
