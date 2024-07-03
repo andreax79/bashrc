c10c02de02f5f548d4cef341452255c1eb462c3d
Andrea Bonomi <andrea.bonomi@gmail.com>
2024-07-03 09:33:25 +0200

    add brew install script

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
 mv ~/.bashrc ~/.bashrc.old 2>/dev/null || true
 ln -s $(readlink -f bashrc) ~/.bashrc
 # Execute install commands
+export NONINTERACTIVE=1
 for install in ~/.config/bash/install.d/* ; do
   $install
 done
